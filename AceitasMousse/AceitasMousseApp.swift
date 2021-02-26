7c42a21140e75bf081ec5e94dda8f6791d0cc559
Vitinho 21 <vbluis.play@gmail.com>
2021-02-26 10:20:41 -0300

    Cria projeto no Xcode

---
+++ AceitasMousse/AceitasMousseApp.swift
@@ -0,0 +1,20 @@
+//
+//  AceitasMousseApp.swift
+//  AceitasMousse
+//
+//  Created by Vítor Bárrios Luís de Albuquerque on 26/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AceitasMousseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
