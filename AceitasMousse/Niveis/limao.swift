0d541082a586503a80aa6d4b25e1c290c4392fa3
Elaine  Cruz <cruzelainef@gmail.com>
2021-03-02 12:06:49 -0300

    logica do nível pronta

--- AceitasMousse/Niveis/limao.swift
+++ AceitasMousse/Niveis/limao.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct limao: View {
+    @State var limaoScale: CGFloat = 20
+    let limaoMaxScale: CGFloat = 20
+    let limaoMinScale: CGFloat = 0.5
+    @State var lastScaleValue: CGFloat = 1.0
+    @GestureState var mag: CGFloat = 1.0
+    
     var body: some View {
         Image("limao")
             .resizable()
             .frame(width: 358, height: 358, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .scaleEffect(setZoom(mag: self.mag))
+            .gesture(
+                MagnificationGesture()
+                        .updating($mag, body: {(value,state, _) in
+                                state = value
+                        
+                        })
+                        .onEnded({value in
+                            self.limaoScale = setZoom(mag: value)
+                        })
+                
+            )
+        
+            .onTapGesture {
+                if(limaoScale==limaoMinScale){
+                    print("toquei")
+
+                }
+            }
+    }
+    
+    func setZoom(mag: CGFloat) -> CGFloat{
+        return max(min(self.limaoScale*mag, self.limaoMaxScale), self.limaoMinScale)
     }
 }
 
 struct limao_Previews: PreviewProvider {
     static var previews: some View {
         limao()
+            .previewDevice("iPhone 11")
     }
 }
