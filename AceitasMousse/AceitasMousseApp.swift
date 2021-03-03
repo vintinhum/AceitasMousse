032590f9d4e92af3e6fd65edd634fb70b2328701
Elaine  Cruz <cruzelainef@gmail.com>
2021-03-03 09:29:53 -0300

    Adding animated background and shake animation for haptic feedback

--- AceitasMousse/AceitasMousseApp.swift
+++ AceitasMousse/AceitasMousseApp.swift
@@ -13,8 +13,9 @@ struct AceitasMousseApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            limao()
         }
     }
 }
